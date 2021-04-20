fb33665eb57e789d03ea5c4d4b7a55ad861bb4e9
3ddelano <3ddelano@gmail.com>
2021-04-20 17:01:36 +0530

    fixed typo in https redirect

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV == 'production') {
 		if (req.headers['x-forwarded-proto'] !== 'https') {
 			return res.redirect(['https://', req.get('Host'), req.url].join(''));
 		}
-		return next;
+		return next();
 	});
 }
 
